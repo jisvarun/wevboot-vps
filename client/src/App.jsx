9228c78f641c0d16ee9ca59f896909975bf9f26c
jisvarun <jisbarunpandit@gmail.com>
2026-05-10 12:18:53 +0530

    first push to production

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState(0)
+
+  useEffect(() => {
+    fetch("http://localhost:4000/api/message")
+    .then((res) => res.json())
+    .then((data) => setMessage(data.message))
+    .catch((err) => {
+      console.error("Error fetching message", err);
+      
+    })
+  }, [])
+
+  return (
+    <>
+      <h1>Welcome to Wevboot frontend </h1>
+      <h2>Data: {message}</h2>
+    </>
+  )
+}
+
+export default App
